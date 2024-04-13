dbe9dc4033b556bc030aa2355da9ff373651d305
SaltyEightLab <hachiman_hachi@icloud.com>
2024-04-14 08:39:38 +0900

    修正

--- api/src/main/java/com/example/api/HelloController.java
+++ api/src/main/java/com/example/api/HelloController.java
@@ -1,38 +1,15 @@
-// package com.example.api;
-
-// import com.example.api.entities.GreetingEntity;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
-// import org.springframework.web.bind.annotation.RestController;
-
-// @RestController
-// @RequestMapping("/api")
-// public class HelloController {
-//     @Autowired
-//     private GreetingRepository repository;
-
-    
-//     @RequestMapping("/hello")
-//     public String hello(
-//             @RequestParam String lang
-//     ) {
-//         GreetingEntity entity = this.repository.findFirstByLang(lang);
-//         return entity.text;
-//     }
-// }
-
 package com.example.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    
     @RequestMapping("/hello")
     public String hello() {
-        return "hello"; // ここで直接 "hello" 文字列を返す
+        return "Hello World!!";
     }
 }
--- api/src/main/java/com/example/api/config/WebMvcConfig.java
+++ api/src/main/java/com/example/api/config/WebMvcConfig.java
@@ -1,10 +1,9 @@
 package com.example.api.config;
 
-// import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
