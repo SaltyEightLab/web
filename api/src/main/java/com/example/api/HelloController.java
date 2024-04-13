95ee40e497b06d874a6820f420e0394941c182ec
SaltyEightLab <hachiman_hachi@icloud.com>
2024-04-14 06:21:07 +0900

    修正

--- api/src/main/java/com/example/api/HelloController.java
+++ api/src/main/java/com/example/api/HelloController.java
@@ -1,23 +1,38 @@
+// package com.example.api;
+
+// import com.example.api.entities.GreetingEntity;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// @RequestMapping("/api")
+// public class HelloController {
+//     @Autowired
+//     private GreetingRepository repository;
+
+    
+//     @RequestMapping("/hello")
+//     public String hello(
+//             @RequestParam String lang
+//     ) {
+//         GreetingEntity entity = this.repository.findFirstByLang(lang);
+//         return entity.text;
+//     }
+// }
+
 package com.example.api;
 
-import com.example.api.entities.GreetingEntity;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
-    @Autowired
-    private GreetingRepository repository;
-
     
     @RequestMapping("/hello")
-    public String hello(
-            @RequestParam String lang
-    ) {
-        GreetingEntity entity = this.repository.findFirstByLang(lang);
-        return entity.text;
+    public String hello() {
+        return "hello"; // ここで直接 "hello" 文字列を返す
     }
 }
