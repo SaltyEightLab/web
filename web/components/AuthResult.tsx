e11e671b92dd5d87dd83f1837f5b178ec5b81c75
SaltyEightLab <hachiman_hachi@icloud.com>
2024-07-14 05:19:55 +0900

    Auth.tsの更新

--- web/components/AuthResult.tsx
+++ web/components/AuthResult.tsx
@@ -6,6 +6,10 @@ const AuthResult = async () => {
         <div className="flex flex-col justify-center items-center">
             <div className="text-2xl font-bold mb-4">AuthResult</div>
             <pre>{JSON.stringify(session, null, 2)}</pre>
+            <div>{process.env.AUTH_GITHUB_ID}</div>
+            <div>{process.env.AUTH_GITHUB_SECRET}</div>
+            <div>{process.env.AUTH_GOOGLE_ID}</div>
+            <div>{process.env.AUTH_GOOGLE_SECRET}</div>
         </div>
     );
 }
