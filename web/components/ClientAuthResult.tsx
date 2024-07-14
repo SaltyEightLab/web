f1e72807ce7493c089d2fc17e83c090823fabd8a
SaltyEightLab <hachiman_hachi@icloud.com>
2024-07-14 12:34:10 +0900

    docker file内でのENV定義

--- web/components/ClientAuthResult.tsx
+++ web/components/ClientAuthResult.tsx
@@ -3,8 +3,18 @@
 import { useSession } from "next-auth/react";
 
 const ClientAuthResult = () => {
-    const { data: session, status } = useSession();
-    return <pre>{JSON.stringify(session, null, 2)}</pre>;
+  const { data: session, status } = useSession();
+  return (
+    <div>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <div>{process.env.AUTH_SECRET}</div>
+      <div>{process.env.AUTH_URL}</div>
+      <div>{process.env.AUTH_GITHUB_ID}</div>
+      <div>{process.env.AUTH_GITHUB_SECRET}</div>
+      <div>{process.env.AUTH_GOOGLE_ID}</div>
+      <div>{process.env.AUTH_GOOGLE_SECRET}</div>
+    </div>
+  );
 };
 
 export default ClientAuthResult;
