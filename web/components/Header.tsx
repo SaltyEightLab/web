526e3995d849a3e4d8bd4fc40a2c6f37ebd2f851
SaltyEightLab <hachiman_hachi@icloud.com>
2024-07-17 05:15:24 +0900

    Auth.js本番実装のためのDockerfileの変更

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -4,7 +4,7 @@ import UserButton from './UserButton';
 const Header = () => {
   return (
     <header className="bg-white p-4 mb-2 shadow-inner text-center flex justify-between items-center h-24">
-      <div className="flex-1">{process.env.AUTH_GITHUB_ID}</div>
+      <div className="flex-1"></div>
       <div className="flex-1 flex justify-center items-center">
         <img src="/logo_green.png" alt="ロゴ" className="inline-block" />
       </div>
